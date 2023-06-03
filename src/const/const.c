3cbc5a8f95cdb9ce45cb20898b6ed4bc2c708bb6
Zackaliok <zackaliok.contact@gmail.com>
2023-06-03 13:17:03 +0200

    Put transparency in floors to make easier to draw in the map

--- src/const/const.c
+++ src/const/const.c
@@ -83,5 +83,4 @@ const SDL_Rect S_RECT_GRAY_POTION = {5*16, 9*16, 16, 16};
 const SDL_Rect S_RECT_GREEN_POTION = {6*16, 9*16, 16, 16};
 const SDL_Rect S_RECT_RED_POTION = {7*16, 9*16, 16, 16};
 const SDL_Rect S_RECT_BLUE_POTION = {8*16, 9*16, 16, 16};
-// TODO : Remplacer une tile par une clé et mettre les bonnes coordonées
 const SDL_Rect S_RECT_KEY = {5*16, 8*16, 16, 16};
